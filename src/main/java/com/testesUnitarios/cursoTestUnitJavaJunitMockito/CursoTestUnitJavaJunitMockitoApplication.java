6d130afa68166312d916a8205a86141df79f9049
AI-QA-paiva <pedropaiva.62124@gmail.com>
2022-11-15 16:29:07 -0300

    primeiro commit

---
+++ src/main/java/com/testesUnitarios/cursoTestUnitJavaJunitMockito/CursoTestUnitJavaJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.testesUnitarios.cursoTestUnitJavaJunitMockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoTestUnitJavaJunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoTestUnitJavaJunitMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testesUnitarios/cursoTestUnitJavaJunitMockito/CursoTestUnitJavaJunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testesUnitarios.cursoTestUnitJavaJunitMockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoTestUnitJavaJunitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
